95979c5b28b5c37c2b9d015dc15a480babec0268
AdamMarczyk <adam.marczyk90@gmail.com>
2018-05-11 21:16:13 +0200

    fix instance

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import ApolloProvider from 'react-apollo';
+import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
