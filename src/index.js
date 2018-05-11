38c27a5872815eadbebe205e56b52a47c965f74c
AdamMarczyk <adam.marczyk90@gmail.com>
2018-05-11 20:57:16 +0200

    add apollo and graphql

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
+import App from './App';
 
-ReactDOM.render(<div />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
