e0a629de2302f2cf102c70fc677274db81cff845
AdamMarczyk <adam.marczyk90@gmail.com>
2018-05-19 12:18:13 +0200

    make navigation a class

--- src/App/Navigation/index.js
+++ src/App/Navigation/index.js
@@ -1,19 +1,40 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
 import * as routes from '../../constants/routes';
 
 import './style.css';
 
-const Navigation = () => (
-  <header className="Navigation">
-    <div className="Navigation-link">
-      <Link to={routes.PROFILE}>Profile</Link>
-    </div>
-    <div className="Navigation-link">
-      <Link to={routes.ORGANIZATION}>Organization</Link>
-    </div>
-  </header>
-);
+class Navigation extends React.Component {
+  state = {
+    organizationName: 'facebook',
+  };
 
-export default Navigation;
+  onOrganizationSearch = value => {
+    this.setState({ organizationName: value });
+  };
+
+  render() {
+    const { location: { pathname } } = this.props;
+
+    return (
+      <header className="Navigation">
+        <div className="Navigation-link">
+          <Link to={routes.PROFILE}>Profile</Link>
+        </div>
+        <div className="Navigation-link">
+          <Link to={routes.ORGANIZATION}>Organization</Link>
+        </div>
+
+        {pathname === routes.ORGANIZATION && (
+          <OrganizationSearch
+            organizationName={this.state.organizationName}
+            onOrganizationSearch={this.onOrganizationSearch}
+          />
+        )}
+      </header>
+    );
+  }
+};
+
+export default withRouter(Navigation);
