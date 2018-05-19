566b43a21d048622347ac0b3c295c34a87a332d5
AdamMarczyk <adam.marczyk90@gmail.com>
2018-05-19 12:00:15 +0200

    use repolist in organization

--- src/Organization/index.js
+++ src/Organization/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 
-import { REPOSITORY_FRAGMENT } from '../Repository';
+import RepositoryList, { REPOSITORY_FRAGMENT } from '../Repository';
+import Loading from '../Loading';
+import ErrorMessage from '../Error';
 
 const GET_REPOSITORIES_OF_ORGANIZATION = gql`
-  query($organizationName: String!) {
+  query($organizationName: String!, $cursor: String!) {
     organization(login: $organizationName) {
-      repositories(first: 5) {
+      repositories(first: 5, after: $cursor) {
         edges {
           node {
             ...repository
           }
+        }
+        pageInfo {
+          endCursor
+          hasNextPage
         }
       }
     }
@@ -26,9 +32,26 @@ const Organization = ({ organizationName }) => (
       organizationName,
     }}
     skip={organizationName === ''}
+    notifyOnNetworkStatusChange={true}
   >
-    {({ data, loading, error }) => {
-      ...
+    {({ data, loading, error, fetchMore }) => {
+      if (error) {
+        return <ErrorMessage error={error} />;
+      }
+
+      const { organization } = data;
+
+      if (loading && !organization) {
+        return <Loading />;
+      }
+
+      return (
+        <RepositoryList
+          loading={loading}
+          repositories={organization.repositories}
+          fetchMore={fetchMore}
+        />
+      );
     }}
   </Query>
 );
