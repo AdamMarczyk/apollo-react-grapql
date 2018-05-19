586c446452b1bb2d9c191ccbcc3a910aa85cc79f
AdamMarczyk <adam.marczyk90@gmail.com>
2018-05-19 11:54:42 +0200

    implement organization component

--- src/Organization/index.js
+++ src/Organization/index.js
@@ -1,5 +1,36 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
 
-const Organization = () => <div>Organization</div>;
+import { REPOSITORY_FRAGMENT } from '../Repository';
+
+const GET_REPOSITORIES_OF_ORGANIZATION = gql`
+  query($organizationName: String!) {
+    organization(login: $organizationName) {
+      repositories(first: 5) {
+        edges {
+          node {
+            ...repository
+          }
+        }
+      }
+    }
+  }
+  ${REPOSITORY_FRAGMENT}
+`;
+
+const Organization = ({ organizationName }) => (
+  <Query
+    query={GET_REPOSITORIES_OF_ORGANIZATION}
+    variables={{
+      organizationName,
+    }}
+    skip={organizationName === ''}
+  >
+    {({ data, loading, error }) => {
+      ...
+    }}
+  </Query>
+);
 
 export default Organization;
