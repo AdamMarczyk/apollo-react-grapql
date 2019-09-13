1f9c6bdfd976f5b82fbb1e7068f24db4ca35021b
Adam Marczyk <adam.marczyk90@gmail.com>
2019-09-13 09:56:29 +0200

    comment add to hooks

--- src/Comment/CommentAdd/index.js
+++ src/Comment/CommentAdd/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
 import gql from 'graphql-tag';
-import { Mutation } from 'react-apollo';
-
+import React from 'react';
+import { useMutation } from 'react-apollo';
 import Button from '../../Button';
-import TextArea from '../../TextArea';
 import ErrorMessage from '../../Error';
+import TextArea from '../../TextArea';
+
 
 const ADD_COMMENT = gql`
   mutation($subjectId: ID!, $body: String!) {
@@ -18,60 +18,51 @@ const ADD_COMMENT = gql`
   }
 `;
 
-class CommentAdd extends React.Component {
-  state = {
-    value: '',
+const CommentAdd = (props) => {
+  const [value, setValue] = React.useState("");
+
+  const onChange = value => {
+    setValue(value);
   };
 
-  onChange = value => {
-    this.setState({ value });
-  };
-
-  onSubmit = (event, addComment) => {
-    addComment().then(() => this.setState({ value: '' }));
+  const onSubmit = (event, addComment) => {
+    addComment().then(() => setValue(""));
 
     event.preventDefault();
   };
 
-  render() {
-    const { value } = this.state;
-    const { issueId } = this.props;
-    return (
-      <Mutation
-        mutation={ADD_COMMENT}
-        variables={{ body: value, subjectId: issueId }}
-        optimisticResponse={{
-          __typename: 'Mutation',
-          addComment: {
-            __typename: 'Comment',
-            body: value,
-            subjectId: issueId,
-          },
-        }}
-        update={(proxy, { data: { addComment } }) => {
-          const data = proxy.readQuery({ query: ADD_COMMENT });
-          data.comments.push(addComment);
-          proxy.writeQuery({ query: ADD_COMMENT, data })
-        }
-        }
-      >
-        {(addComment, { data, loading, error }) => (
-          <div>
-            {error && <ErrorMessage error={error} />}
+  const { issueId } = props;
+  const [addComment, { data, loading, error }] = useMutation(ADD_COMMENT, {
+    variables: { body: value, subjectId: issueId },
+    optimisticResponse: {
+      __typename: 'Mutation',
+      addComment: {
+        __typename: 'Comment',
+        body: value,
+        subjectId: issueId,
+      },
+    },
+    update: (proxy, { data: { addComment } }) => {
+      const data = proxy.readQuery({ query: ADD_COMMENT });
+      data.comments.push(addComment);
+      proxy.writeQuery({ query: ADD_COMMENT, data })
+    }
+  });
 
-            <form onSubmit={e => this.onSubmit(e, addComment)}>
-              <TextArea
-                value={value}
-                onChange={e => this.onChange(e.target.value)}
-                placeholder="Leave a comment"
-              />
-              <Button type="submit">Comment</Button>
-            </form>
-          </div>
-        )}
-      </Mutation>
-    );
-  }
+  return (
+    <div>
+      {error && <ErrorMessage error={error} />}
+
+      <form onSubmit={e => onSubmit(e, addComment)}>
+        <TextArea
+          value={value}
+          onChange={e => onChange(e.target.value)}
+          placeholder="Leave a comment"
+        />
+        <Button type="submit">Comment</Button>
+      </form>
+    </div>
+  );
 };
 
 export default CommentAdd;
